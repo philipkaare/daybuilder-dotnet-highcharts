70ad41ccb4d73fe98fc6621fa375fb330ab7bfd0
Philip Løventoft <pkl@daybuilder.dk>
2014-03-04 13:06:12 +0100

    First commit. Fork of dotnet-highcharts from CodePlex.

---
+++ DotNet.Highcharts/Enums/ChartTypes.cs
@@ -0,0 +1,29 @@
+﻿namespace DotNet.Highcharts.Enums
+{
+    #region DefaultSeriesType enum
+
+    /// <summary>
+    /// The default series type for the chart.
+    /// </summary>
+    public enum ChartTypes
+    {
+        Line,
+        Spline,
+        Area,
+        Areaspline,
+        Column,
+        Bar,
+        Pie,
+        Scatter,
+        Arearange,
+        Areasplinerange,
+        Columnrange,
+        Gauge,
+        Boxplot,
+        Waterfall,
+        Funnel,
+        Bubble
+    }
+
+    #endregion
+}
